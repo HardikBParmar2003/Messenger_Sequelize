c5f2914162472bbe65bbac43315ba596e84b1a54
Hardik Parmar <hardik.parmar@esparkbizmail.com>
2025-06-27 12:36:55 +0530

    Remove database_url and update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,14 +4,27 @@ import { User, Otp, Group, Member, Chat, Status, Call, Role, Permission, RolePer
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-  dialect: "postgres",
-  models: [User, Otp, Group, Member, Chat, Status, Call, Role, Permission, RolePermission],
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // for Railway SSL
-    },
-  },
-});
+const databaseUrl = process.env.DATABASE_URL;
+
+export const sequelize = databaseUrl
+  ? new Sequelize(databaseUrl, {
+      dialect: "postgres",
+      models: [User, Otp, Group, Member, Chat, Status, Call, Role, Permission, RolePermission],
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize({
+      dialect: "postgres",
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER, 
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      models: [User, Otp, Group, Member, Chat, Status, Call, Role, Permission, RolePermission],
+      logging: false,
+    });
+
