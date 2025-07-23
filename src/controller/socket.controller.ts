489f85a2c4d0e4957944c361d7ea53d6811ca324
Hardik Parmar <hardik.parmar@esparkbizmail.com>
2025-07-23 17:23:25 +0530

    feature:group room id created

--- src/controller/socket.controller.ts
+++ src/controller/socket.controller.ts
@@ -1,16 +1,38 @@
 import { socketService } from "../services/socket.service";
 export default function socketTest(ioe: any) {
   ioe.on("connection", (socket: any) => {
-    socket.on("send message", async (sender_id:number,receiver_id: number, message: string) => {
-      const group_id = 0;
-      const chatData = await socketService.addMessage(sender_id,receiver_id, message,group_id);
-      ioe.emit("send message back", chatData);
+    socket.on("joinGroups", (groupIds: number[]) => {
+      groupIds.forEach((group_id) => {
+        socket.join(String(group_id));
+      });
     });
 
-    socket.on("send group message", async (sender_id:number,group_id: number, message: string) => {
-      const receiver_id = 0;
-      const chatData = await socketService.addMessage(sender_id,receiver_id, message,group_id);
-      ioe.emit("send group message back", chatData);
-    });
+    socket.on(
+      "send message",
+      async (sender_id: number, receiver_id: number, message: string) => {
+        const group_id = 0;
+        const chatData = await socketService.addMessage(
+          sender_id,
+          receiver_id,
+          message,
+          group_id
+        );
+        ioe.emit("send message back", chatData);
+      }
+    );
+
+    socket.on(
+      "send group message",
+      async (sender_id: number, group_id: number, message: string) => {
+        const receiver_id = 0;
+        const chatData = await socketService.addMessage(
+          sender_id,
+          receiver_id,
+          message,
+          group_id
+        );
+        ioe.to(String(group_id)).emit("send group message back", chatData);
+      }
+    );
   });
 }
